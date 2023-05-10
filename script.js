b67e3f1eecdbed63cadfce58e6f2cbd3e32dd8e1
GHJBaek <jbaek.dev@gmail.com>
2023-05-10 17:14:45 -0400

    dropdown menu update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 const navButton = document.querySelector('.menuToggle');
 const navMenu = document.getElementById('navMenu');
-const navLinks = document.querySelectorAll('a');
+const navLinks = document.querySelectorAll('.dropLink');
 
 navButton.addEventListener('click', function () {
     navButton.classList.toggle('is-active');
@@ -17,12 +17,38 @@ navButton.addEventListener('click', function () {
 
 });
 
-const closeMenu =() => {
-    navLinks.addEventListener('click', function () {
-        if(navMenu.classList.contains('dropDownMenu')){
-            navMenu.classList.remove('dropDownMenu');
-            navMenu.classList.add('openNav');
-        }
-})};
 
-closeMenud();
+// close menu when link is clicked
+const closeMenu = () => {
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            if (navMenu.classList.contains('dropDownMenu')) {
+                navMenu.classList.remove('dropDownMenu');
+                navMenu.classList.add('openNav');
+                navButton.classList.toggle('is-active');
+            }
+        })
+    })
+}
+
+const handleSubmit = () => {
+
+    const handleForm = document.querySelector('.contactForm');
+
+    handleForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => navigate("/thank-you/"))
+            .catch((error) => alert(error));
+    });
+}
+handleSubmit();
+closeMenu();
